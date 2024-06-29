cb4107686d2b89772815dac4e6316d1aa277d576
Rohit Manohar <manoharrohit372@gmail.com>
2024-06-29 19:30:52 +0530

    done

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,27 +7,29 @@ const Footer = () => {
       <div className="container mx-auto px-4 text-center md:text-left">
         <div className="md:flex md:justify-between md:items-center">
           <div>
-            
-            <p>No &copy; copyright issues.</p>
-            <p>Feel free to copy. If you need any help, ping me!</p>
+            <p className="text-xl mt-0md:mb-4">No &copy; copyright issues.</p>
+            <p className="mb-4">Feel free to copy. If you need any help, ping me!</p>
           </div>
-          <div className="flex justify-center md:justify-end space-x-6 mb-4 mt-4 md:mt-0">
-          <p className="">You can find me everywhere</p>
-            <a href="https://github.com/rohitmanohar2108" target="_blank" rel="noopener noreferrer" className="hover:text-blue-800 transition-colors">
-              <FaGithub className="text-2xl" />
-            </a>
-            <a href="https://www.linkedin.com/in/rohit-manohar-80b949207/" target="_blank" rel="noopener noreferrer" className="hover:text-blue-800 transition-colors">
-              <FaLinkedin className="text-2xl" />
-            </a>
-            <a href="https://www.instagram.com/rohitmanohar2108/" target="_blank" rel="noopener noreferrer" className="hover:text-blue-800 transition-colors">
-              <FaInstagram className="text-2xl" />
-            </a>
-            <a href="https://twitter.com/imrohit372" target="_blank" rel="noopener noreferrer" className="hover:text-blue-800 transition-colors">
-              <FaTwitter className="text-2xl" />
-            </a>
-            <a href="mailto:rohitmanohar2108@gmail.com" className="hover:text-blue-800 transition-colors">
-              <FaEnvelope className="text-2xl" />
-            </a>
+          <div className='text-4xl mr-28 font-dancing-script'>Rohit Manohar</div>
+          <div className="flex flex-col items-center md:items-end">
+            <p className="mb-4">You can find me everywhere</p>
+            <div className="flex justify-center md:justify-end space-x-6 mb-4">
+              <a href="https://github.com/rohitmanohar2108" target="_blank" rel="noopener noreferrer" className="hover:text-blue-800 transition-colors">
+                <FaGithub className="text-2xl" />
+              </a>
+              <a href="https://www.linkedin.com/in/rohit-manohar-80b949207/" target="_blank" rel="noopener noreferrer" className="hover:text-blue-800 transition-colors">
+                <FaLinkedin className="text-2xl" />
+              </a>
+              <a href="https://www.instagram.com/rohitmanohar2108/" target="_blank" rel="noopener noreferrer" className="hover:text-blue-800 transition-colors">
+                <FaInstagram className="text-2xl" />
+              </a>
+              <a href="https://twitter.com/imrohit372" target="_blank" rel="noopener noreferrer" className="hover:text-blue-800 transition-colors">
+                <FaTwitter className="text-2xl" />
+              </a>
+              <a href="mailto:rohitmanohar2108@gmail.com" className="hover:text-blue-800 transition-colors">
+                <FaEnvelope className="text-2xl" />
+              </a>
+            </div>
           </div>
         </div>
       </div>
