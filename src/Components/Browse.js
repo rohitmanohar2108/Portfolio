3f1de400a31a89fbab01c4aafdc73411834f2960
Rohit Manohar <manoharrohit372@gmail.com>
2024-06-29 12:15:35 +0530

    done

--- src/Components/Browse.js
+++ src/Components/Browse.js
@@ -5,6 +5,7 @@ import { gsap } from "gsap";
 import { motion } from "framer-motion";
 // Import your new Lottie animation JSON file (replace with your own animation)
 import animationData from "../Animation/animation.json";
+import { FaGithub, FaLinkedin, FaInstagram, FaTwitter } from "react-icons/fa";
 
 const Portfolio = () => {
   const defaultOptions = {
@@ -63,7 +64,7 @@ const Portfolio = () => {
           <motion.div className="mt-8" whileHover={{ scale: 1.1 }}>
             <Lottie options={defaultOptions} height={400} width={400} />
           </motion.div>
-          <h1 className="text-7xl text-pink-600 font-bold mt-4 " ref={textRef}>
+          <h1 className="text-7xl text-pink-600 font-bold mt-4" ref={textRef}>
             {splitText("Rohit Manohar")}
           </h1>
           <motion.h2
@@ -73,6 +74,67 @@ const Portfolio = () => {
             Hi, I am Rohit, a Computer Science Student at NITK
           </motion.h2>
         </div>
+        <motion.div
+          className="mt-16 p-8 bg-gray-800 rounded-lg shadow-lg flex items-center"
+          whileHover={{ scale: 1.05 }}
+        >
+          <img
+            src="https://media-sin6-2.cdn.whatsapp.net/v/t61.24694-24/426021548_762504602111403_3961460611984576911_n.jpg?ccb=11-4&oh=01_Q5AaIA4o3BITHKBtjO9y596jqVhz6qE02EjFppCgCIVcvZn1&oe=66869524&_nc_sid=e6ed6c&_nc_cat=105"
+            alt="Rohit"
+            className="w-56 h-56 rounded-full mr-8"
+          />
+          <div>
+            <h3 className="text-2xl text-white font-bold mb-4">About Me</h3>
+            <p className="text-white text-xl">
+              I am a Computer Science Student at NITK. I have a deep passion for
+              coding and constantly strive to enhance my skills by learning new
+              technologies. I love tackling challenging problems and finding
+              innovative solutions. My areas of interest include web
+              development, Programming and many more. I am always eager to
+              collaborate on exciting projects and learn from others in the
+              field. When I am not coding, I enjoy exploring the latest tech
+              trends and participating in hackathons and coding competitions.
+              Let's connect and create something amazing together! Whether it's
+              building a new app, diving into a complex algorithm, or simply
+              exchanging knowledge, I am excited to engage with like-minded
+              individuals and contribute to the tech community.
+            </p>
+            <div className="flex mt-4 space-x-4 z-20 relative">
+                <a
+                  href="https://github.com/rohitmanohar2108"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-white"
+                >
+                  <FaGithub className="text-4xl hover:text-gray-400 transition-colors" />
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/rohit-manohar-80b949207/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-white"
+                >
+                  <FaLinkedin className="text-4xl hover:text-gray-400 transition-colors" />
+                </a>
+                <a
+                  href="https://www.instagram.com/rohitmanohar2108/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-white"
+                >
+                  <FaInstagram className="text-4xl hover:text-gray-400 transition-colors" />
+                </a>
+                <a
+                  href="https://twitter.com/imrohit372"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-white"
+                >
+                  <FaTwitter className="text-4xl hover:text-gray-400 transition-colors" />
+                </a>
+              </div>
+          </div>
+        </motion.div>
       </div>
     </div>
   );
