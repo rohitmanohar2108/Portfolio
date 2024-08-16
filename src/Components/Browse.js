140440c74364085e4ca43cd7fd7484b524336c5a
Rohit Manohar <manoharrohit372@gmail.com>
2024-08-16 14:27:33 +0530

    done

--- src/Components/Browse.js
+++ src/Components/Browse.js
@@ -376,6 +376,7 @@ const Portfolio = () => {
             </motion.div>
           </div>
         </motion.div>
+        
 
         <motion.div
           className="mt-16"
@@ -548,7 +549,7 @@ const Portfolio = () => {
         >
           <Lottie options={defaultOptionsfour} height={400} width={400} />
           <button
-            className="text-4xl  text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 shadow-lg shadow-pink-500/50 dark:shadow-lg dark:shadow-pink-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 font-bold py-2 px-6 ml-3 rounded-lg  transition-colors shadow-xl"
+            className="text-4xl  text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 shadow-xl shadow-pink-500/50 dark:shadow-lg dark:shadow-pink-800/80 font-bold rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 font-bold py-2 px-6 ml-3 rounded-lg  transition-colors shadow-xl"
             onClick={toggleForm}
           >
             Contact Me
